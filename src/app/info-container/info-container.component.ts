3433dfede17bdd612d06e5c8238a6cf6f82220c4
JacqKiefner15 <jacqkiefner@gmail.com>
2020-11-01 20:50:06 -0600

    groundwork from class 10-29

---
+++ src/app/info-container/info-container.component.ts
@@ -0,0 +1,17 @@
+import { MatCardModule } from '@angular/material/card';
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-info-container',
+  templateUrl: './info-container.component.html',
+  styleUrls: ['./info-container.component.scss']
+})
+export class InfoContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
