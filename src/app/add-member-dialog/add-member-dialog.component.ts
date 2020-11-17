5304550a2874daf7fb753799361df39f4297eb20
smsievers <seth_sievers@yahoo.com>
2020-11-16 19:55:26 -0600

    done did the array slicing and fancifying

--- src/app/add-member-dialog/add-member-dialog.component.ts
+++ src/app/add-member-dialog/add-member-dialog.component.ts
@@ -1,10 +1,11 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, ViewChild } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Team } from 'src/app/team';
 import { TEAMS } from './../mock-teams';
 import {MAT_DIALOG_DATA} from '@angular/material/dialog';
 import { TeamsService } from '../teams.service';
 import { PicsumService } from '../picsum.service';
+import {MatPaginator, PageEvent} from '@angular/material/paginator';
 
 @Component({
   selector: 'app-add-member-dialog',
@@ -15,11 +16,23 @@ export class AddMemberDialogComponent implements OnInit {
   teams: Team[] = TEAMS;
   positions = ['Software Engineer', 'Quality Engineer', 'Team Anchor'];
   images = [];
-  
+  startPoint = 0;
+  selectedImage: any;
+
   constructor(private picSumService: PicsumService,private teamService: TeamsService, @Inject(MAT_DIALOG_DATA) public data: {name: string}) { }
 
   ngOnInit(): void {
     this.teamService.getAllTeams().subscribe((teams: Team[])=> {this.teams = teams})
     this.picSumService.getPictures().subscribe((images: any[])=> {this.images = images})
   }
+
+  selected(image) {
+    this.selectedImage = image
+  
+    }
+  pageChange($event: PageEvent) {
+    this.startPoint = $event.pageIndex * 5
+  }
+
+  
 }
