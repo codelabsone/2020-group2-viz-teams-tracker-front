77446195c971109d15d17d63485954b77343adae
smsievers <seth_sievers@yahoo.com>
2020-11-30 19:06:21 -0600

    a

--- src/app/mock-files/mock-teams.ts
+++ src/app/mock-files/mock-teams.ts
@@ -1,4 +1,4 @@
-import {Team} from '../models/team-interface'
+import {Team} from '../team';
 // import { MEMBERS } from '../mock-files/mock-members';
 
 
@@ -6,7 +6,6 @@ export const TEAMS: Team[]=[
   {
   id: 1,
   name: "P2P",
-  description: "baloney potato salad",
   members:[
   { id: 1, name: "Roger Beasley", pic: "http://placekitten.com/100/100", position:'Software engineer'},
   { id: 2, name: "Bob", pic: "http://placekitten.com/100/100", position:'Software engineer'},
@@ -14,19 +13,18 @@ export const TEAMS: Team[]=[
   { id: 4, name: "Molly", pic: "http://placekitten.com/100/100", position:'Software engineer' },
   { id: 5, name: "Seth", pic: "http://placekitten.com/100/100",  },
   { id: 6, name: "Shawn", pic: "http://placekitten.com/100/100" },
-  { id: 7, name: "Francee", pic: "http://placekitten.com/100/100" },
-  { id: 8, name: "Jacqueline", pic: "http://placekitten.com/100/100" },
-  { id: 9, name: "Phyllis", pic: "http://placekitten.com/100/100" },
-  { id: 10, name: "Joe", pic: "http://placekitten.com/100/100" },
-  { id: 11, name: "Bill", pic: "http://placekitten.com/100/100" },
-  { id: 12, name: "Sue", pic: "http://placekitten.com/100/100" },
+  // { id: 7, name: "Francee", pic: "http://placekitten.com/100/100" },
+  // { id: 8, name: "Jacqueline", pic: "http://placekitten.com/100/100" },
+  // { id: 9, name: "Phyllis", pic: "http://placekitten.com/100/100" },
+  // { id: 10, name: "Joe", pic: "http://placekitten.com/100/100" },
+  // { id: 11, name: "Bill", pic: "http://placekitten.com/100/100" },
+  // { id: 12, name: "Sue", pic: "http://placekitten.com/100/100" },
     ]
   },
   // {
   {
   id: 2,
   name: "nema-codes",
-  description: "Gold Team rules",
   members:[
   { id: 1, name: "Steve", pic: "http://placekitten.com/100/100", position:'Software engineer'},
   { id: 2, name: "Bob", pic: "http://placekitten.com/100/100", position:'Software engineer'},
