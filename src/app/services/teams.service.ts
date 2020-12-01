ff06ec01dbb7f828ba3c83b43c1dfb2d25b324e9
MollyJW <mollyjwhitener@gmail.com>
2020-11-30 18:58:39 -0600

    idk

--- src/app/services/teams.service.ts
+++ src/app/services/teams.service.ts
@@ -1,14 +1,11 @@
-import { map, tap } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
-import { Observable, of } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { Team } from '../models/team';
 import { TEAMS } from '../mock-files/mock-teams';
 import { Router } from "@angular/router";
-import {BehaviorSubject} from 'rxjs';
-
-
+import { map, tap } from 'rxjs/operators';
+import { BehaviorSubject, Observable, of } from 'rxjs';
 
 
 @Injectable({
@@ -16,6 +13,7 @@ import {BehaviorSubject} from 'rxjs';
 })
 export class TeamsService {
 // private teamUrl = './mock-teams'
+selectedTeam: BehaviorSubject<Team> = new BehaviorSubject(null)
 private apiUrl: string
 
 constructor(
@@ -40,12 +38,48 @@ constructor(
 
    getAllTeams() {
     return this.http.get<any>(`${this.apiUrl}/index`)
+    .pipe(
+      tap(data => console.log('All: ' + JSON.stringify(data))),
    }
 
    addNewTeam(params) {
     return this.http.post<any>(`${this.apiUrl}/create`, params);
    }
 
-
-
   }
+
+  // @Injectable({
+//   providedIn: 'root'
+// })
+// export class TeamsService {
+// private teamUrl = './mock-teams'
+// baseUr
+//   selectedTeam: BehaviorSubject<Team> = new BehaviorSubject(null)
+//   constructor( private http: HttpClient) {
+//    }
+
+//    getTeambyId(params) {
+//      return this.http.get<any>(`${this.teamUrl}/show?id=${params.id}`);
+//    }
+
+//    getTeam( id: number): Observable<Team> {
+//      return null
+//     //  return this.getAllTeams()
+//     //  .pipe(
+//     //   map((thisTeam: Team) => thisTeam.find(p => p.id === id))
+//     // );
+//    }
+
+//    getAllTeams(): Observable <Team[]> {
+//     return this.http.get<Team[]>(this.teamUrl)
+//     .pipe(
+//       tap(data => console.log('All: ' + JSON.stringify(data))),
+
+//     );
+//     return of(TEAMS)
+//    }
+
+
+
+  // }
+
